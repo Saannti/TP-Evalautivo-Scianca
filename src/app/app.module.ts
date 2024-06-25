7c8ff8cf265303ca81927d4bff9691d9cd9de998
nombre <sciancasantino@gmail.com>
2024-06-25 10:55:49 -0300

    CReacion de components, enviroments, incio con sus principales objetos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
+
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
